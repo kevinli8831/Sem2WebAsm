3bf169ca688fdbf9325c288b74d4bfe6a3ccd5ca
Ashleyng131 <ashleyng131@gmail.com>
2021-04-21 14:50:53 +0800

    Others

---
+++ Contactus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact / Support us</title>
+</head>
+
+<body>
+
+    <br>
+    <h1>CONTACT US</h1>
+
+    <h4><pre>
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    E-mail :
+	info@hkyptal.org.hk
+	Office :  3560 4866
+	Fax: 3753 0645
+	Mobile: 6600 8288
+	Address :
+	PO Box 83099, Concorde Road Post Office
+	Room 817, 8/F, Youth Square, 238 Chai Wan Road, Chai Wan, Hong Kong
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+    </pre></h4>
+
+    <b><h4>
+    Register Now! Have Questions or Suggestions?
+	</h4></b>
+
+
+
+
+
+	<br>
+    <input type="submit" value="send">
+    <br>
+</body>
+</html>
